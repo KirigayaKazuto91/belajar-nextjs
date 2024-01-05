2a246a501217d832020f7c6a6a8736d78e3322ca
KirigayaKazuto91 <mike_shadowz91@protonmail.com>
2024-01-05 16:16:28 +0700

    Link, Navigation(useParams, useRouter)

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,20 +1,53 @@
+"use client";
+import { link } from "fs";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+  const router = useRouter();
   return (
-    <nav className="flex bg-purple-500 py-4 px-2">
-      <h1 className="mr-5">Navbar</h1>
-      <ul className="flex">
-        <Link href="/">
-          <li className="mr-5 cursor-pointer hover:bg-purple-800">Home</li>
-        </Link>
-        <Link href="/about">
-          <li className="mr-5 cursor-pointer hover:bg-purple-800">About</li>
-        </Link>
-        <Link href="/about/profile">
-          <li className="mr-5 cursor-pointer hover:bg-purple-800">Profile</li>
-        </Link>
-      </ul>
+    <nav className="flex bg-purple-500 py-4 px-2 justify-between">
+      <div className="flex">
+        <h1 className="mr-5">Navbar</h1>
+        <ul className="flex">
+          <Link href="/">
+            <li
+              className={`mr-5 ${
+                pathname === "/" ? "text-blue-800" : "text-white"
+              } cursor-pointer hover:bg-purple-800`}
+            >
+              Home
+            </li>
+          </Link>
+          <Link href="/about">
+            <li
+              className={`mr-5 ${
+                pathname === "/about" ? "text-blue-800" : "text-white"
+              } cursor-pointer hover:bg-purple-800`}
+            >
+              About
+            </li>
+          </Link>
+          <Link href="/about/profile">
+            <li
+              className={`mr-5 ${
+                pathname === "/about/profile" ? "text-blue-800" : "text-white"
+              } cursor-pointer hover:bg-purple-800`}
+            >
+              Profile
+            </li>
+          </Link>
+        </ul>
+      </div>
+      <div>
+        <button
+          className="bg-white rounded-md px-3 h-7 text-sm text-black"
+          onClick={() => router.push("/login")}
+        >
+          Login
+        </button>
+      </div>
     </nav>
   );
 }
--- src/app/template.tsx
+++ src/app/template.tsx
@@ -5,8 +5,8 @@ const Template = ({ children }: { children: React.ReactNode }) => {
   const [state, setState] = useState(0);
   return (
     <div>
-      <h1>Hello World {state}</h1>
-      <button onClick={() => setState(state + 1)}>Click me</button>
+      {/* <h1>Hello World {state}</h1>
+      <button onClick={() => setState(state + 1)}>Click me</button> */}
       {children}
     </div>
   );
